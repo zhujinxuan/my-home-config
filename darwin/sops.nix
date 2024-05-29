e323f2dffe6842b0ecea8bba13ee8a2f37b028e7
zhujinxuan <zhujinxuan@gmail.com>
2024-05-29 19:02:50 -0400

    Password stored

--- darwin/sops.nix
+++ darwin/sops.nix
@@ -11,12 +11,12 @@
 
   sops = {
     age.keyFile = "/Users/jinxuanzhu/.config/sops/age/keys.txt";
-    defaultSopsFile = ./secrets/secrets.yaml;
-    secrets.id_rsa = {
-      # %r gets replaced with a runtime directory, use %% to specify a '%'
-      # sign. Runtime dir is $XDG_RUNTIME_DIR on linux and $(getconf
-      # DARWIN_USER_TEMP_DIR) on darwin.
-      path = "%r/test.txt";
-    };
+    defaultSopsFile = ../secrets/secrets.yaml;
+    # secrets.id_rsa = {
+    #   # %r gets replaced with a runtime directory, use %% to specify a '%'
+    #   # sign. Runtime dir is $XDG_RUNTIME_DIR on linux and $(getconf
+    #   # DARWIN_USER_TEMP_DIR) on darwin.
+    #   path = "%r/test1.txt";
+    # };
   };
 }
